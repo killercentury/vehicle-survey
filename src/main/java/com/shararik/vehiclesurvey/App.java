c11be1e628981d80e8e5436e6e0247e088ad8006
Kira Cheung <killercentury@gmail.com>
2015-04-26 00:57:17 +1000

    be able to load the data file

--- src/main/java/com/shararik/vehiclesurvey/App.java
+++ src/main/java/com/shararik/vehiclesurvey/App.java
@@ -1,8 +1,32 @@
 package com.shararik.vehiclesurvey;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class App {
+
+    private static final String DATA_FILE_NAME = "data.txt";
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        readData();
+        System.out.println("Data has been loaded");
+    }
+
+    public static void readData() {
+        String filePathStr = App.class.getClassLoader().getResource(DATA_FILE_NAME).getPath();
+        Path path = Paths.get(filePathStr);
+        Stream<String> lines;
+        try {
+            lines = Files.lines(path);
+            lines.forEach(System.out::println);
+            lines.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
