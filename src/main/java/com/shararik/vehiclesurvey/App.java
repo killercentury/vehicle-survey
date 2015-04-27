e8e25fa64b92418db1d64644ae71a0581451eeb2
Kira Cheung <killercentury@gmail.com>
2015-04-27 21:37:56 +1000

    remove side effect of readData method

--- src/main/java/com/shararik/vehiclesurvey/App.java
+++ src/main/java/com/shararik/vehiclesurvey/App.java
@@ -1,6 +1,5 @@
 package com.shararik.vehiclesurvey;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,23 +9,26 @@ import java.util.stream.Stream;
 public class App {
 
     private static final String DATA_FILE_NAME = "data.txt";
+    public static Stream<String> data;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        readData();
+        data = readData(DATA_FILE_NAME);
         System.out.println("Data has been loaded");
     }
 
-    public static void readData() {
-        String filePathStr = App.class.getClassLoader().getResource(DATA_FILE_NAME).getPath();
+    public static Stream<String> readData(String dataFileName) {
+        String filePathStr = App.class.getClassLoader().getResource(dataFileName).getPath();
         Path path = Paths.get(filePathStr);
         Stream<String> lines;
         try {
             lines = Files.lines(path);
             lines.forEach(System.out::println);
             lines.close();
+            return lines;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 }
