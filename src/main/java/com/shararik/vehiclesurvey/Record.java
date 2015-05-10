b306b6c12938201427009e0474156713c909614b
Kira Cheung <killercentury@gmail.com>
2015-05-11 00:14:41 +1000

    add record class

---
+++ src/main/java/com/shararik/vehiclesurvey/Record.java
@@ -0,0 +1,30 @@
+package com.shararik.vehiclesurvey;
+
+/**
+ * Created by kira on 10/05/15.
+ */
+public class Record {
+    private Hose hose;
+    private int time;
+
+    public Record(Hose hose, int time) {
+        this.hose = hose;
+        this.time = time;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public Hose getHose() {
+        return hose;
+    }
+
+    public void setHose(Hose hose) {
+        this.hose = hose;
+    }
+}
