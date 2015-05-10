5ce99d2728e59ba01524aa0a89cc89858b3eb494
Kira Cheung <killercentury@gmail.com>
2015-05-10 22:54:05 +1000

    add functions for finding the last data point of the day

--- src/main/java/com/shararik/vehiclesurvey/App.java
+++ src/main/java/com/shararik/vehiclesurvey/App.java
@@ -4,18 +4,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class App {
 
     private static final String DATA_FILE_NAME = "data.txt";
-    public static Stream<String> data;
+    public static String[] dataArray;
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
-        try (Stream<String> lines = readData(DATA_FILE_NAME)) {
-            lines.forEach(System.out::println);
+        try (Stream<String> dataStream = readData(DATA_FILE_NAME)) {
+            dataArray = dataStream.toArray(String[]::new);
             System.out.println("Data has been loaded");
+
+            int[] timeSeriesData = transformToTimeSeriesData(Arrays.stream(dataArray));
+
+            getDayEndPoints(timeSeriesData).forEach(System.out::println);
+            getDayEndPoints(timeSeriesData).forEach(i -> System.out.println(timeSeriesData[i]));
         }
     }
 
@@ -24,6 +32,30 @@ public class App {
         Path path = Paths.get(filePathStr);
         Stream<String> lines = Files.lines(path);
         return lines;
+    }
+
+    // Transform data to time series data
+    public static int[] transformToTimeSeriesData(Stream<String> dataStream) {
+        return dataStream.mapToInt(l -> Integer.parseInt(l.substring(1))).toArray();
+    }
+
+    // Find all the index of last data point of the day
+    public static List<Integer> getDayEndPoints(int[] timeSeriesData) {
+
+        List<Integer> dayEndPoints = new ArrayList<Integer>();
+        IntStream.range(1, timeSeriesData.length - 1).filter(i -> {
+            if (timeSeriesData[i - 1] > timeSeriesData[i]) {
+                return true;
+            } else {
+                return false;
+            }
+        }).forEach(i -> dayEndPoints.add(i - 1));
+
+        // The last point provided by the data may not be the actual end point for the day.
+        // But we assume it as the last point for analytics purpose.
+        dayEndPoints.add(timeSeriesData.length - 1);
+
+        return dayEndPoints;
     }
 
     public static long countSouthBound(Stream<String> lines) {
--- src/test/java/com/shararik/vehiclesurvey/AppTest.java
+++ src/test/java/com/shararik/vehiclesurvey/AppTest.java
@@ -3,6 +3,7 @@ package com.shararik.vehiclesurvey;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -13,8 +14,28 @@ import static org.junit.Assert.assertEquals;
 public class AppTest {
     @Test
     public void countData() throws IOException {
-        Stream<String> lines = App.readData("data.txt");
-        long num = lines.count();
+        Stream<String> dataStream = App.readData("data.txt");
+        long num = dataStream.count();
         assertEquals(67296, num);
+    }
+
+    @Test
+    public void testTimeSeriesDataTransformation() throws IOException {
+        Stream<String> dataStream = App.readData("data.txt");
+        int[] timeSeriesData = App.transformToTimeSeriesData(dataStream);
+        assertEquals(67296, timeSeriesData.length);
+    }
+
+    @Test
+    public void testDayEndPointsDetection() throws IOException {
+        Stream<String> dataStream = App.readData("data.txt");
+        int[] timeSeriesData = App.transformToTimeSeriesData(dataStream);
+        List<Integer> dayEndPoints = App.getDayEndPoints(timeSeriesData);
+        assertEquals(5, App.getDayEndPoints(timeSeriesData).size());
+        assertEquals(86351672, timeSeriesData[dayEndPoints.get(0)]);
+        assertEquals(86381837, timeSeriesData[dayEndPoints.get(1)]);
+        assertEquals(86382072, timeSeriesData[dayEndPoints.get(2)]);
+        assertEquals(86174912, timeSeriesData[dayEndPoints.get(3)]);
+        assertEquals(86389454, timeSeriesData[dayEndPoints.get(4)]);
     }
 }
